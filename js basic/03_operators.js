810dc8b2dfa6c4e82e56593447dbf43662263d4d
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2024-06-19 16:46:35 +0530

    done

---
+++ js basic/03_operators.js
@@ -0,0 +1,98 @@
+// JavaScript Assignment Operators
+
+
+// Operator      Example   Same As
+// =	          x = y	    x = y
+// +=	          x += y	x = x + y
+// -=             x -= y	x = x - y
+// *=             x *= y    x = x * y
+// /=	          x /= y    x = x / y
+// %=	          x %= y	x = x % y
+// **=	          x **= y	x = x ** y
+
+
+// Assignment (=)
+
+let x = 10;
+console.log("value of x:",x)
+
+
+// Addition operator (+=)
+
+let a = 5;
+a +=3;
+console.log("value of a:",a)
+
+
+//subtraction (-=)
+
+let b = 5;
+b -=3;
+console.log("value of b:",b)
+
+//Multiplication operator (*=)
+
+let c = 7;
+c *= 8;
+console.log("value of c :",c)
+
+//Division Operator (/=)
+
+let d =  20;
+d /= 5;
+console.log("value of d:",d)
+
+// Remainder Operator (%=)
+
+let e = 15;
+e %=4;
+
+console.log("value of e:",e)
+
+
+//Exponentiation Operator (**=)
+
+let f = 3;
+f **=3;
+
+console.log("value of f :",f)
+
+
+// Bitwise AND Operator (&=)
+
+let g = 5;
+g &= 3;
+
+console.log("value of g:",g)
+
+
+//Bitwise OR Assignment (|=)
+
+let h = 5;
+h |=3;
+
+console.log("value of h: ", h)
+
+// Bitwise XOR Assignment (^=)
+
+let i = 5;
+i ^3;
+console.log("value of i:",i)
+
+// Left Shift Assignment (<<=)
+
+let j = 5;
+j <<=1;
+console.log("value of j:",j)
+
+// Right Shift Assignment (>>=)
+
+let k = 5;
+k >>1;
+console.log("value of k:",k)
+
+// Unsigned Right Shift Assignment (>>>=)
+
+let l = 5;
+l >>>1;
+console.log("value of l:",l)
---
+++ js basic/test.js
@@ -0,0 +1,4 @@
+var text =  "Nilesh"
+console.log(text);
+var next = "Hello,world"
+console.log(next);
