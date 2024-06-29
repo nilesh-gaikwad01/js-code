25f3821491d3b92cdffac8e6c4e2a2ad14e23c1d
nilesh-gaikwad01 <nileshgaik1212@gmail.com>
2024-06-29 08:13:15 +0530

    second commit

---
+++ js basic/05_Num.js
@@ -0,0 +1,9 @@
+let score = 10
+console.log(score)
+
+const balance = new Number(100) // Js Number Object use imp- use for console and learn its various functions
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+console.log("what is Needed For Happiness");
